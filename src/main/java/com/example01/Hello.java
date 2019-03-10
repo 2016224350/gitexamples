676ab532098c83f490776209b4a52ac1f4d0efe4
2016224350 <2817643255@qq.com>
2019-03-10 15:56:38 +0800

    bugfix/0

--- src/main/java/com/example01/Hello.java
+++ src/main/java/com/example01/Hello.java
@@ -3,6 +3,9 @@ package com.example01;
 public class Hello {
     public void hello(){}
     public void bug(){
-        int num = 1/0;
+        int num = 10;
+    }
+    public void bugfix(){
+        System.out.println("bug is fixed");
     }
 }
