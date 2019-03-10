1f4f5ef66852c39e124a895d14e436e80f71cb03
2016224350 <2817643255@qq.com>
2019-03-10 15:06:36 +0800

    feature/0

--- src/main/java/com/example01/Hello.java
+++ src/main/java/com/example01/Hello.java
@@ -4,5 +4,9 @@ public class Hello {
     public void hello(){}
     public void bug(){
         int num = 1/0;
+        int num = num + 10;
+    }
+    public void fun(){
+        System.out.println("this is a fun test");
     }
 }
