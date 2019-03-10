980b70f2f01c88c4962b565146c5ce0bb27cf7fb
2016224350 <2817643255@qq.com>
2019-03-10 15:58:50 +0800

    bugfix/1

--- src/main/java/com/example01/Hello.java
+++ src/main/java/com/example01/Hello.java
@@ -4,9 +4,12 @@ public class Hello {
     public void hello(){}
     public void bug(){
         int num = 1/0;
-        int num = num + 10;
+        num = num + 10;
     }
     public void fun(){
         System.out.println("this is a fun test");
+    }
+    public void findNothing(){
+        System.out.println("nothing is finded");
     }
 }
